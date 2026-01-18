0cdd4dde930d1291f828aa88cbc055447ed74db6
Ruud Tammel <ruud.tammel@gmail.com>
2026-01-18 16:04:37 +0200

    Tweaks and fixes

--- Assets/Scripts/Board/TurnController.cs
+++ Assets/Scripts/Board/TurnController.cs
@@ -43,6 +43,9 @@ public class TurnController : MonoBehaviour
 
     void Update()
     {
+        if (PauseManager.IsPaused)
+            return;
+
         if (follower == null)
         {
             follower = FindObjectOfType<WaypointFollower>();
